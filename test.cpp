3dfb577172f62007508a5a1d1aa639ebc7048fd6
3000ye <hcg2924239214@163.com>
2023-09-22 18:06:34 +0800

    第一次更新

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+void solve() {
+  cout << "Hello World!" << endl;
+}
+
+int main() {
+  cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(false);
+  solve();
+  return 0;
+}

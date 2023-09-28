bda266c0dea1857b53e114ee097c2cfd76ba438d
3000ye <hcg2924239214@163.com>
2023-09-28 15:22:47 +0800

    test commit

---
+++ AtCoder/ABC-C/abc-303c.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+void solve() {
+    
+}
+
+int main() {
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    solve();
+    return 0;
+}

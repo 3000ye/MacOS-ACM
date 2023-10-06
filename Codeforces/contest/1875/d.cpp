53fefcd684fffc4b99ba94579d244235a41b433e
3000ye <hcg2924239214@163.com>
2023-10-06 15:19:53 +0800

    新增了AtCoder ABC-C的记录

---
+++ Codeforces/contest/1875/d.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+void solve() {
+    
+}
+
+int main() {
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    int T; cin >> T;
+    while (T -- ) solve();
+    return 0;
+}
